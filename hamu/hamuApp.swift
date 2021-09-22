06c23771b30c9480070169480d9e0ecc3c2ac7ff
cdyas <dyaca941@student.otago.ac.nz>
2021-09-22 21:29:49 +1200

    Initial Commit

---
+++ hamu/hamuApp.swift
@@ -0,0 +1,17 @@
+//
+//  hamuApp.swift
+//  hamu
+//
+//  Created by Caitlin Dyas on 22/09/21.
+//
+
+import SwiftUI
+
+@main
+struct hamuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
